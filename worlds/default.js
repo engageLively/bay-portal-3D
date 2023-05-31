7de50faec9dcef45067815847eeb72d7bd37dc11
Rick McGeer <rick@mcgeer.com>
2023-05-31 10:51:53 -0700

    removed synchronous load

--- worlds/default.js
+++ worlds/default.js
@@ -39,7 +39,7 @@
                     placeholderColor: 0x808080,
                     placeholderOffset: [0, 0, 0],
                     // behaviorModules: ['LoadCards']
-                    behaviorModules: ['SynchronousCardLoader']
+                    // behaviorModules: ['SynchronousCardLoader']
                 }
             },
             {
